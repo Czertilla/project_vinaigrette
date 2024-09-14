b39f53a928ffab95096a80ec9676ab41f31f9260
Czertilla <blaztermapskorsak@gmail.com>
2024-09-14 03:51:28 +0300

    Initial

---
+++ android/src/main/java/com/czertilla/project_vinaigrette/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.czertilla.project_vinaigrette.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.czertilla.project_vinaigrette.Main;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new Main(), configuration);
+    }
+}
