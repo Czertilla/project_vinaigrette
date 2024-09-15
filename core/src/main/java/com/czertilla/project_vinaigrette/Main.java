ac40caf0ed6bb47692c50937669de15cca7cfade
Czertilla <blaztermapskorsak@gmail.com>
2024-09-15 22:17:54 +0300

    feature(Main) add resize mthd class Main:     add method resize:         add resize viewport implementation

--- core/src/main/java/com/czertilla/project_vinaigrette/Main.java
+++ core/src/main/java/com/czertilla/project_vinaigrette/Main.java
@@ -35,6 +35,11 @@ public class Main extends ApplicationAdapter {
     }
 
     @Override
+    public void resize (int width, int height) {
+        viewport.update(width, height, true);
+    }
+
+    @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
