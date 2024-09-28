ec15da013b4be8693152cf8a89474f1b2855bcb5
Czertilla <blaztermapskorsak@gmail.com>
2024-09-28 14:54:56 +0300

    refactor(Main) replace extends edit class Main:     replace ApplicationAdapter by Game in extends

--- core/src/main/java/com/czertilla/project_vinaigrette/Main.java
+++ core/src/main/java/com/czertilla/project_vinaigrette/Main.java
@@ -1,14 +1,12 @@
 package com.czertilla.project_vinaigrette;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
+public class Main extends Game {
     private SpriteBatch batch;
     private Texture image;
 
@@ -30,5 +28,6 @@ public class Main extends ApplicationAdapter {
     public void dispose() {
         batch.dispose();
         image.dispose();
+        super.dispose();
     }
 }
