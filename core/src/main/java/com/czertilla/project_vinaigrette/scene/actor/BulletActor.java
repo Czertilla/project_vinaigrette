76fde426d96794b4ba6481f0690b17bf0329879e
wallrat1 <gleblox337@mail.ru>
2024-10-05 17:55:00 +0300

    feature(BulletActor) add class

---
+++ core/src/main/java/com/czertilla/project_vinaigrette/scene/actor/BulletActor.java
@@ -0,0 +1,71 @@
+package com.czertilla.project_vinaigrette.scene.actor;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Polygon;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+public class BulletActor extends Actor {
+    private TextureRegion region;
+    private Polygon collisionPolygon; // Полигон для коллизии
+    private float speed;
+    private float direction; // Угол направления пули
+
+    public BulletActor(Texture texture, float startX, float startY, float direction, float speed) {
+        this.region = new TextureRegion(texture);
+        this.direction = direction;
+        this.speed = speed;
+
+        // Устанавливаем размеры пули на основе текстуры
+        setSize(region.getRegionWidth(), region.getRegionHeight());
+
+        // Устанавливаем начальную позицию пули
+        setPosition(startX - getWidth() / 2, startY - getHeight() / 2); // центрирование пули
+
+        // Создаем полигон для коллизии (прямоугольник)
+        collisionPolygon = new Polygon(new float[]{
+            0, 0,                   // нижний левый угол
+            getWidth(), 0,           // нижний правый угол
+            getWidth(), getHeight(), // верхний правый угол
+            0, getHeight()           // верхний левый угол
+        });
+
+        // Устанавливаем исходную позицию полигона
+        collisionPolygon.setPosition(getX(), getY());
+        collisionPolygon.setOrigin(getWidth() / 2, getHeight() / 2);
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+
+        // Движение пули
+        float velocityX = MathUtils.cosDeg(direction) * speed;
+        float velocityY = MathUtils.sinDeg(direction) * speed;
+        moveBy(velocityX * delta, velocityY * delta);
+
+        // Обновляем позицию полигона при движении
+        collisionPolygon.setPosition(getX(), getY());
+        collisionPolygon.setRotation(getRotation());
+
+        // Если пуля выходит за пределы экрана, удаляем её
+        if (getX() < -getWidth() || getX() > getStage().getWidth() || getY() < -getHeight() || getY() > getStage().getHeight()) {
+            remove(); // Удаляем пулю из сцены
+        }
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        batch.setColor(getColor());
+        batch.draw(region, getX(), getY(), getOriginX(), getOriginY(),
+            getWidth(), getHeight(), getScaleX(), getScaleY(), getRotation());
+    }
+
+    // Возвращает полигон для проверки столкновений
+    public Polygon getCollisionPolygon() {
+        return collisionPolygon;
+    }
+}
+
