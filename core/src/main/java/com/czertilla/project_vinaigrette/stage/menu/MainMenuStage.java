add3de2d4fb4106d307ab2516baaf290f774d43e
Czertilla <blaztermapskorsak@gmail.com>
2024-10-07 15:46:19 +0300

    feature(MainMenuStage) add add class

---
+++ core/src/main/java/com/czertilla/project_vinaigrette/stage/menu/MainMenuStage.java
@@ -0,0 +1,51 @@
+package com.czertilla.project_vinaigrette.stage.menu;
+
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.czertilla.project_vinaigrette.screen.menu.MainMenu;
+
+public class MainMenuStage extends BaseMenuStage{
+    private static MainMenuStage instance;
+    public static MainMenuStage getInstance(MainMenu menu){
+        if (instance == null){
+            instance = new MainMenuStage();
+        }
+        instance.menu = menu;
+        return instance;
+    }
+
+    public void show(){
+        super.show();
+
+        createContinueButton();
+        createSettingsButton();
+    }
+
+    private void createSettingsButton() {
+        TextButton button = getNewTextButton("Settings");
+
+        button.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                menu.setStage(SettingsMenuStage.getInstance(menu));
+            }
+        });
+
+        initButton(button);
+    }
+
+    private void createContinueButton() {
+        TextButton button = getNewTextButton("Continue");
+
+        button.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+//                TODO implement Continue Button click trigger
+            }
+        });
+
+        initButton(button);
+    }
+
+}
