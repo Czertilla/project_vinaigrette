56d96820337003a2a90f4fc9812923e9ac6ec38c
Czertilla <blaztermapskorsak@gmail.com>
2024-10-09 23:09:25 +0300

    chore(BaseMenu) clean imports

--- core/src/main/java/com/czertilla/project_vinaigrette/screen/menu/BaseMenu.java
+++ core/src/main/java/com/czertilla/project_vinaigrette/screen/menu/BaseMenu.java
@@ -1,22 +1,10 @@
 package com.czertilla.project_vinaigrette.screen.menu;
 
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.czertilla.project_vinaigrette.Main;
 import com.czertilla.project_vinaigrette.stage.menu.BaseMenuStage;
 
