3096919e7184a978c6b04c1b1591b4013486aca8
Czertilla <blaztermapskorsak@gmail.com>
2024-10-19 02:28:26 +0300

    feature(R.path) integrate path properties

--- core/src/main/java/com/czertilla/project_vinaigrette/utils/R.java
+++ core/src/main/java/com/czertilla/project_vinaigrette/utils/R.java
@@ -1,8 +1,20 @@
 package com.czertilla.project_vinaigrette.utils;
 
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.I18NBundle;
+
 public class R {
     public static class path {
+        private static I18NBundle bundle = I18NBundle.createBundle(
+            new FileHandle("strings/paths")
+        );
 
+        public static final String MAIN_MENU_BACKGROUND = bundle.get("MAIN_MENU_BACKGROUND");
+        public static final String BUTTON_DRAWABLE_REGION = bundle.get("BUTTON_DRAWABLE_REGION");
+        public static final String PIXEL_FONT = bundle.get("PIXEL_FONT");
+        public static final String STRINGS_HANDLE = bundle.get("STRINGS_HANDLE");
+        public static final String CHARSET_HANDLE = bundle.get("CHARSET_HANDLE");
+        public static final String PATHS_HANDLE = bundle.get("PATHS_HANDLE");
     }
 
     public static final class id {
@@ -17,5 +29,6 @@ public class R {
         public static final String SWITCH_ON = "BUTTONS/MENU/SWITCH/ON";
         public static final String SWITCH_OFF = "BUTTONS/MENU/SWITCH/OFF";
         public static final String SCALE = "BUTTONS/MENU/SCALE";
+        public static final String CHARSET = "CHARSET";
     }
 }
