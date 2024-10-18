7191ac9d001acec8ec936140487a294ad16f5a5d
Czertilla <blaztermapskorsak@gmail.com>
2024-10-19 02:04:52 +0300

    feature(utils) add class R

---
+++ core/src/main/java/com/czertilla/project_vinaigrette/utils/R.java
@@ -0,0 +1,21 @@
+package com.czertilla.project_vinaigrette.utils;
+
+public class R {
+    public static class path {
+
+    }
+
+    public static final class id {
+        public static final String CONTINUE = "BUTTONS/MENU/CONTINUE";
+        public static final String NEW_GAME = "BUTTONS/MENU/NEW_GAME";
+        public static final String SETTINGS = "BUTTONS/MENU/SETTINGS";
+        public static final String BACK = "BUTTONS/MENU/BACK";
+        public static final String QUIT = "BUTTONS/MENU/QUIT";
+        public static final String VIDEO = "BUTTONS/MENU/VIDEO";
+        public static final String DEBUG = "BUTTONS/MENU/DEBUG";
+        public static final String FULLSCREEN = "BUTTONS/MENU/FULLSCREEN";
+        public static final String SWITCH_ON = "BUTTONS/MENU/SWITCH/ON";
+        public static final String SWITCH_OFF = "BUTTONS/MENU/SWITCH/OFF";
+        public static final String SCALE = "BUTTONS/MENU/SCALE";
+    }
+}
