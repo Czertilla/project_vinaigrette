2c9dcc09325685da288775b1188bb03a7ba81d7c
Czertilla <blaztermapskorsak@gmail.com>
2024-10-19 03:17:16 +0300

    feature(R) add keys for C

--- core/src/main/java/com/czertilla/project_vinaigrette/utils/R.java
+++ core/src/main/java/com/czertilla/project_vinaigrette/utils/R.java
@@ -9,26 +9,38 @@ public class R {
             new FileHandle("strings/paths")
         );
 
-        public static final String MAIN_MENU_BACKGROUND = bundle.get("MAIN_MENU_BACKGROUND");
-        public static final String BUTTON_DRAWABLE_REGION = bundle.get("BUTTON_DRAWABLE_REGION");
-        public static final String PIXEL_FONT = bundle.get("PIXEL_FONT");
-        public static final String STRINGS_HANDLE = bundle.get("STRINGS_HANDLE");
-        public static final String CHARSET_HANDLE = bundle.get("CHARSET_HANDLE");
-        public static final String PATHS_HANDLE = bundle.get("PATHS_HANDLE");
+        public static final String
+            LAYOUT_HANDLE = bundle.get("LAYOUT_HANDLE"),
+            VIDEO_HANDLE = bundle.get("VIDEO_HANDLE"),
+            MAIN_MENU_BACKGROUND = bundle.get("MAIN_MENU_BACKGROUND"),
+            BUTTON_DRAWABLE_REGION = bundle.get("BUTTON_DRAWABLE_REGION"),
+            PIXEL_FONT = bundle.get("PIXEL_FONT"),
+            STRINGS_HANDLE = bundle.get("STRINGS_HANDLE"),
+            CHARSET_HANDLE = bundle.get("CHARSET_HANDLE"),
+            PATHS_HANDLE = bundle.get("PATHS_HANDLE");
     }
 
     public static final class id {
-        public static final String CONTINUE = "BUTTONS/MENU/CONTINUE";
-        public static final String NEW_GAME = "BUTTONS/MENU/NEW_GAME";
-        public static final String SETTINGS = "BUTTONS/MENU/SETTINGS";
-        public static final String BACK = "BUTTONS/MENU/BACK";
-        public static final String QUIT = "BUTTONS/MENU/QUIT";
-        public static final String VIDEO = "BUTTONS/MENU/VIDEO";
-        public static final String DEBUG = "BUTTONS/MENU/DEBUG";
-        public static final String FULLSCREEN = "BUTTONS/MENU/FULLSCREEN";
-        public static final String SWITCH_ON = "BUTTONS/MENU/SWITCH/ON";
-        public static final String SWITCH_OFF = "BUTTONS/MENU/SWITCH/OFF";
-        public static final String SCALE = "BUTTONS/MENU/SCALE";
-        public static final String CHARSET = "CHARSET";
+        public static final String
+            CONTINUE = "BUTTONS/MENU/CONTINUE",
+            NEW_GAME = "BUTTONS/MENU/NEW_GAME",
+            SETTINGS = "BUTTONS/MENU/SETTINGS",
+            BACK = "BUTTONS/MENU/BACK",
+            QUIT = "BUTTONS/MENU/QUIT",
+            VIDEO = "BUTTONS/MENU/VIDEO",
+            DEBUG = "BUTTONS/MENU/DEBUG",
+            FULLSCREEN = "BUTTONS/MENU/FULLSCREEN",
+            SWITCH_ON = "BUTTONS/MENU/SWITCH/ON",
+            SWITCH_OFF = "BUTTONS/MENU/SWITCH/OFF",
+            SCALE = "BUTTONS/MENU/SCALE",
+            CHARSET = "CHARSET",
+            LANG_CODES = "languages",
+            FRAMES_LOCK = "FRAMES_LOCK",
+            SCALE_ARRAY = "SCALE_ARRAY",
+            MENU_BUTTONS_WIDTH = "MENU_BUTTONS_WIDTH",
+            MENU_BUTTONS_HEIGHT = "MENU_BUTTONS_HEIGHT",
+            FONT_SIZE = "FONT_SIZE",
+            DEFAULT_WIDTH = "DEFAULT_WIDTH",
+            DEFAULT_HEIGHT = "DEFAULT_HEIGHT";
     }
 }
