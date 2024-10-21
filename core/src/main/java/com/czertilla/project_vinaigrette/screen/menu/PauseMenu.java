16a5aa02c9a21fe65af31343d14d051240725c5e
Czertilla <blaztermapskorsak@gmail.com>
2024-10-21 16:33:16 +0300

    feature(menu) add PauseMenu class

---
+++ core/src/main/java/com/czertilla/project_vinaigrette/screen/menu/PauseMenu.java
@@ -0,0 +1,39 @@
+package com.czertilla.project_vinaigrette.screen.menu;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.czertilla.project_vinaigrette.Main;
+import com.czertilla.project_vinaigrette.screen.game.MainGame;
+import com.czertilla.project_vinaigrette.stage.menu.MainMenuStage;
+import com.czertilla.project_vinaigrette.stage.menu.PauseMenuStage;
+import com.czertilla.project_vinaigrette.stage.scene.GameScene;
+import com.czertilla.project_vinaigrette.utils.R;
+
+public class PauseMenu extends BaseMenu{
+    public PauseMenu(Main game) {
+        super(game);
+
+        background = new Texture(R.path.MAIN_MENU_BACKGROUND);
+
+        setDefaultStage();
+    }
+
+    @Override
+    public void setDefaultStage() {
+        setStage(PauseMenuStage.getInstance(this));
+    }
+
+    public void resumeGame(){
+        game.setScreen(new MainGame(game));
+    }
+    public void exit() {
+        game.setScreen(new MainMenu(game));
+    }
+
+    public void dispose() {
+        super.dispose();
+        background.dispose();
+//        backgroundMusic.dispose();
+//        clickSound.dispose();
+    }
+
+}
