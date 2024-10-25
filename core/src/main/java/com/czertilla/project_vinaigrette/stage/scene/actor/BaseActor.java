c69ef0ffe813bfbab7253647c1da0ce54c89dc60
Czertilla <blaztermapskorsak@gmail.com>
2024-10-26 01:22:43 +0300

    feature(BaseActor) add getCenter edit class BaseActor:     add method getCenter

--- core/src/main/java/com/czertilla/project_vinaigrette/stage/scene/actor/BaseActor.java
+++ core/src/main/java/com/czertilla/project_vinaigrette/stage/scene/actor/BaseActor.java
@@ -3,6 +3,7 @@ package com.czertilla.project_vinaigrette.stage.scene.actor;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Polygon;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class BaseActor extends Actor {
@@ -15,6 +16,14 @@ public class BaseActor extends Actor {
         this.region = region;
         super.setSize(region.getRegionWidth(), region.getRegionHeight());
 
+    }
+
+    public Vector3 getCenter(){
+        return new Vector3(
+            getX()+getWidth()/2,
+            getY()+getHeight()/2,
+            0
+        );
     }
 
     public Polygon getBoundingBox() {
