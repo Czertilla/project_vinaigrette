f6cf42dff12e7bbb89211799449fac8e67e8bb41
Czertilla <blaztermapskorsak@gmail.com>
2024-10-26 04:20:08 +0300

    feature(PlayerActor) add damage method

--- core/src/main/java/com/czertilla/project_vinaigrette/stage/scene/actor/PlayerActor.java
+++ core/src/main/java/com/czertilla/project_vinaigrette/stage/scene/actor/PlayerActor.java
@@ -1,11 +1,15 @@
 package com.czertilla.project_vinaigrette.stage.scene.actor;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Polygon;
+import com.czertilla.project_vinaigrette.utils.C;
 
 public class PlayerActor extends BaseActor {
+
+    private float dmgTime = 0;
 
     public PlayerActor(TextureRegion region) {
         super(region);
@@ -56,6 +60,12 @@ public class PlayerActor extends BaseActor {
     public void drawBoundingBox(ShapeRenderer shapeRenderer) {
         float[] vertices = boundingBox.getTransformedVertices();
         shapeRenderer.polygon(vertices);  // Отрисовка полигона
+    }
+
+    public void damage(float dmg){
+        if (dmgTime > 0) return;
+        dmgTime = C.DAMAGE_COOLDOWN;
+        setColor(Color.RED);
     }
 
     public void act(float delta) {
