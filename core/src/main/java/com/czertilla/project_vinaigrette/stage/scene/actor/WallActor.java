d4c4d76cba64b5756c59596bd1c8524f427002c6
Czertilla <blaztermapskorsak@gmail.com>
2024-11-06 17:25:14 +0300

    feature(WallActor) add move method edit class WallActor:     remove useful imports     edit constructor:     replace methods setWidth, setHeight by method setSize     add method move:         chose direction to move collides actor:         1) (28-36) difference between 4 90 degrees sides of rectangle to get more close to direction to collides actor         2) (37-43) move actor in chosen direction with step=1 until it collides with wall

--- core/src/main/java/com/czertilla/project_vinaigrette/stage/scene/actor/WallActor.java
+++ core/src/main/java/com/czertilla/project_vinaigrette/stage/scene/actor/WallActor.java
@@ -1,19 +1,45 @@
 package com.czertilla.project_vinaigrette.stage.scene.actor;
 
-import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.physics.box2d.Box2D;
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.czertilla.project_vinaigrette.utils.Movable;
 
 public class WallActor extends BaseActor{
     static final Texture empty = new Texture("ui/empty.png");
     public WallActor(float width, float height) {
         super(new TextureRegion(empty));
-        setWidth(width);
-        setHeight(height);
+        setSize(width, height);
+    }
+
+    public void move(BaseActor other) {
+        Vector3 center = getCenter();
+        float
+            cx = center.x,
+            cy = center.y;
+        Vector3 direction = other.getCenter().sub(center).setLength(1);
+        float dst = Float.POSITIVE_INFINITY;
+        Vector3 axis = new Vector3(0, 0, 1);
+        Vector3 dir = new Vector3(1, 0, 0).rotate(
+            axis,
+            getRotation()
+        );
+        Vector3 result = new Vector3(0, 0, 0);
+        for (int i = 0; i < 4; i++, dir.rotate(axis, 90)){
+            float mlp = ((i % 2 == 1) ? getHeight() : getWidth()) / 2;
+            float len = dir.cpy().sub(direction).len() * mlp;
+            if (len < dst) {
+                result.set(dir);
+                dst = len;
+            }
+        }
+        result.setLength(1);
+        do {
+            other.moveBy(result.x, result.y);
+            other.updateBoundingBox();
+        } while (Intersector.overlapConvexPolygons(
+            this.getBoundingBox(),
+            other.getBoundingBox()
+        ));
     }
 }
