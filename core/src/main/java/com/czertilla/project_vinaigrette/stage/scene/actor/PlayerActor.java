1524259db8f74af54f975ebbd087658e601fd3bb
Czertilla <blaztermapskorsak@gmail.com>
2024-11-16 17:20:48 +0300

    feature(PlayerActor) add updateAnimation

--- core/src/main/java/com/czertilla/project_vinaigrette/stage/scene/actor/PlayerActor.java
+++ core/src/main/java/com/czertilla/project_vinaigrette/stage/scene/actor/PlayerActor.java
@@ -2,7 +2,10 @@ package com.czertilla.project_vinaigrette.stage.scene.actor;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.utils.Null;
@@ -24,7 +27,7 @@ public class PlayerActor extends BaseActor implements Movable {
     private float friction = 35f;
 
     public PlayerActor(TextureRegion region) {
-        super(region,"ui/Player.atlas");
+        super(region,new TextureAtlas("ui/Player.atlas"));
         ammo = new Ammo(100,100,100);
         maxSpeed = C.PLAYER_MAX_SPEED;
         velocity = new Vector3();
@@ -85,6 +88,52 @@ public class PlayerActor extends BaseActor implements Movable {
         return weapon==other;
     }
 
+    public void updateAnimation(){
+        String currStateName = animationHandler.stateName;
+
+        Vector3 screenCoords = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+        Vector3 destination = getStage().getViewport().unproject(screenCoords).sub(getCenter());
+        String nextState = getStateName(destination);
+        if (!nextState.equals(currStateName)) animationHandler.setAnimation(nextState, 6);
+        animationHandler.setFrameDuration(
+            velocity.isZero() ? C.DEFAULT_PLAYER_FRAME_DUR :
+                C.DEFAULT_PLAYER_FRAME_DUR / velocity.len() * maxSpeed
+        );
+        Vector2
+            dest2 = new Vector2(destination.x, destination.y),
+            vels2 = new Vector2(velocity.x, velocity.y);
+        float angle = vels2.angleDeg(dest2);
+        float marg = C.REVERSE_ANIMATION_ANGLE;
+        animationHandler.animation.setPlayMode(
+            angle > 180 - marg && angle < 180 + marg ? Animation.PlayMode.LOOP_REVERSED
+                : Animation.PlayMode.LOOP
+        );
+    }
+
+    private String getStateName(Vector3 destination) {
+        String nextState = C.State.DOWN;
+        if (!destination.isZero()) {
+            if (destination.x >= 0){
+                if (destination.y > destination.x)
+                    nextState = C.State.UP;
+                else if (destination.y > -destination.x)
+                    nextState = C.State.RIGHT;
+                else
+                    nextState = C.State.DOWN;
+            }
+            else {
+                if (destination.y < destination.x)
+                    nextState = C.State.DOWN;
+                else if (destination.y < -destination.x)
+                    nextState = C.State.LEFT;
+                else
+                    nextState = C.State.UP;
+            }
+        }
+        if (velocity.isZero()) nextState = C.State.IDLE_PREFIX + nextState;
+        return nextState;
+    }
+
     @Override
     void processCollision(Actor other) {
         super.processCollision(other);
@@ -119,6 +168,7 @@ public class PlayerActor extends BaseActor implements Movable {
     public void update(float delta) {
         float frictionForce = friction * C.G;
         velocity.setLength(Math.max(0, velocity.len()-frictionForce*delta));
+        updateAnimation();
         velocity.mulAdd(acceleration, delta);
         moveBy(velocity.x * delta, velocity.y * delta);
     }
