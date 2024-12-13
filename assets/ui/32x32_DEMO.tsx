e5029467bbfa544780670bd4947a36839f7cf1eb
wallrat1 <gleblox337@mail.ru>
2024-12-14 01:03:24 +0300

    PlayerAtlas

---
+++ assets/ui/32x32_DEMO.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="32x32_DEMO" tilewidth="32" tileheight="32" tilecount="1040" columns="40">
+ <image source="32x32_DEMO.png" width="1280" height="832"/>
+</tileset>
