2a34f473ab21138d4f7fe91ea10afdd6af13c7e1
rekuiemuu <ayakakparovoziktomas@gmail.com>
2024-12-21 04:36:33 +0300

    add image menu and buttonfix

---
+++ assets/test_gg/gg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="gg" tilewidth="80" tileheight="64" tilecount="170" columns="10">
+ <image source="../../../../Users/rekuiemuu/Downloads/GandalfHardcore 14x Arm Layers/GandalfHardcore Warrior.png" width="800" height="1088"/>
+</tileset>
