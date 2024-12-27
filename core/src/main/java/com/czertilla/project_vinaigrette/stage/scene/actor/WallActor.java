92b4340200346ca13c54f299f3217e17db4552da
Czertilla <blaztermapskorsak@gmail.com>
2024-12-27 21:05:53 +0300

    perf(WallActor) refactor method move

--- core/src/main/java/com/czertilla/project_vinaigrette/stage/scene/actor/WallActor.java
+++ core/src/main/java/com/czertilla/project_vinaigrette/stage/scene/actor/WallActor.java
@@ -30,26 +30,38 @@ public class WallActor extends BaseActor {
     public void move(BaseActor other) {
         Vector3 wallCenter = getCenter();
         float[] wallWorldVertices = getBoundingBox().getTransformedVertices();
-        int wallVerticesCount = getBoundingBox().getVertexCount();
         float
             cx = wallCenter.x,
             cy = wallCenter.y;
         Vector3 otherCenter = new Vector3(
-            other.getBoundingBox().getOriginX(),
-            other.getBoundingBox().getOriginY(),
+            other.getBoundingBox().getX() +
+                other.getBoundingBox().getBoundingRectangle().getWidth() / 2,
+            other.getBoundingBox().getY() +
+                other.getBoundingBox().getBoundingRectangle().getHeight() / 2,
             0);
         Vector3 otherDirection = otherCenter.cpy().sub(wallCenter).setLength(1);
         float otherDirAngle = MathUtils.atan2(otherDirection.y, otherDirection.x);
         Vector3 result = new Vector3(1, 0, 0);
-        for (int i=0; i <= wallVerticesCount; i += 2){
+        for (int i=0; i <= getBoundingBox().getVertexCount()*2; i += 2){
             float
-                x1 = wallWorldVertices[i % wallVerticesCount],
-                y1 = wallWorldVertices[(i + 1) % wallVerticesCount],
-                x2 = wallWorldVertices[(i + 2) % wallVerticesCount],
-                y2 = wallWorldVertices[(i + 3) % wallVerticesCount];
+                x1 = wallWorldVertices[i % wallWorldVertices.length],
+                y1 = wallWorldVertices[(i + 1) % wallWorldVertices.length],
+                x2 = wallWorldVertices[(i + 2) % wallWorldVertices.length],
+                y2 = wallWorldVertices[(i + 3) % wallWorldVertices.length];
             float angle1 = MathUtils.atan2(y1 - cy, x1 - cx);
             float angle2 = MathUtils.atan2(y2 - cy, x2 - cx);
-            if (angle1 < otherDirAngle && otherDirAngle <= angle2) {
+            float otherAngle;
+            if (Math.abs(angle2 - angle1) > MathUtils.PI){
+                float added = angle1 + MathUtils.PI * 2;
+                angle1 = angle2;
+                angle2 = added;
+                otherAngle = otherDirAngle + MathUtils.PI * 2 * ((otherDirAngle < 0)?1:0);
+            }
+            else
+                otherAngle = otherDirAngle;
+            if (
+                angle1 < otherAngle && otherAngle <= angle2
+            ) {
                 float
                     dx = x2 - x1,
                     dy = y2 - y1;
@@ -57,7 +69,7 @@ public class WallActor extends BaseActor {
                     p = new Vector2(-dy, dx),
                     ac = new Vector2(cx - x1, cy - y1);
                 result.set(p.x, p.y, 0);
-                if (p.dot(ac) < 0)
+                if (p.dot(ac) > 0)
                     result.scl(-1);
                 result.setLength(1);
                 break;
